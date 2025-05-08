2d951bac288d226f208ac7cf45cbd451a6bee398
himjohn <john.polonia25@compscihigh.org>
2025-05-08 01:46:43 +0000

    Rate Teach Done

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -1,2 +1,21 @@
 // Add an event listener to the form that upon submits creates a new rating
 // When the rating is created redirect to the ratings page
+const rating = document.querySelector("form")
+rating.addEventListener("submit"), async (e)=>{
+    e.preventDefault()
+    const rateData = new rating(rate)
+const reqBody = ratingSchema.ratingEntries(rateData)
+const response = await fetch("/add/rating",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+ },
+    body:json.Stringify(reqBody)
+})
+
+const data = await response.json()
+
+console.log(data)
+window.location.href = "/ratings"
+
+}
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,2 +1,22 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
+const teachForm = document.querySelector("form")
+
+teachForm.addEventListener("submit", async (e)=>{
+   e.preventDefault()
+
+   const formData = new FormData(teachForm)
+   const reqBody = teacherSchema.teachFormEntries(formData)
+
+   const response = await fetch("/add/teacher", {
+    method: "POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body: json.Stringify(reqBody)
+
+})
+   const data = await response.json()
+   console.log(data)
+    windows.location.href = "/"
+})
